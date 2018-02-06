7e9e7b3fdadd004580da35eff9c77f634325a50f
Paulo McNally <paulomcnally@gmail.com>
2018-02-06 16:32:46 -0600

    New changes

--- app/src/main/java/com/uca/apps/perrito/ui/activities/MainActivity.kt
+++ app/src/main/java/com/uca/apps/perrito/ui/activities/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.uca.apps.perrito
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.uca.apps.perrito.R.id.email
 
-import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        //setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        save()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,6 +33,16 @@ class MainActivity : AppCompatActivity() {
         return when(item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun save() {
+        val name = findViewById<TextInputEditText>(R.id.name)
+        val email = findViewById<TextInputEditText>(R.id.email)
+        val code = findViewById<TextInputEditText>(R.id.code)
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            Toast.makeText(applicationContext, name.text, Toast.LENGTH_LONG).show()
         }
     }
 }
